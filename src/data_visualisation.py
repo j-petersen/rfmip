eaa38952743cd195d50bee530d9c8ffc633f010d
roemulus <roemerflo@gmail.com>
2022-07-27 11:19:33 +0200

    separate plots for selected profiles, using heigth as vertical grid

--- src/data_visualisation.py
+++ src/data_visualisation.py
@@ -1,4 +1,3 @@
-# %%
 import os
 import pyarts
 import numpy as np
@@ -8,51 +7,94 @@ import matplotlib.pyplot as plt
 from experiment_setup import read_exp_setup
 import helping_functions as hf
 
-def plot_irradiance(irrad, lam_grid, exp_setup) -> None:
+
+def plot_irradiance(heights, irrad, lam_grid, exp_setup, index) -> None:
     ty.plots.styles.use(["typhon", "typhon-dark"])
     fig, ax = plt.subplots(1, 1, figsize=(12, 9))
     for i, wavelength in enumerate(lam_grid):
-        # color = hf.wavelength2rgb(wavelength)
         color = hf.rgb4uvvis(wavelength)
-        ax.plot(irrad[i, :, 0], np.arange(np.shape(irrad)[1]), color=color, marker='v', label=str(int(np.round(wavelength)))+' nm')
-        ax.plot(irrad[i, :, 1], np.arange(np.shape(irrad)[1]), color=color, marker='^')
+        ax.plot(
+            irrad[i, :, 0],
+            heights / 1e3,
+            color=color,
+            marker="v",
+            label=str(int(np.round(wavelength))) + " nm",
+        )
+        ax.plot(irrad[i, :, 1], heights / 1e3, color=color, marker="^")
 
-    ax.set_xlabel(r'spectral irradiance / W$\,$m$^{-2}\,$nm$^{-1}$')
-    ax.set_ylabel('layer / 1')
+    ax.set_xlabel(r"spectral irradiance / W$\,$m$^{-2}\,$nm$^{-1}$")
+    ax.set_ylabel("height / km")
     ax.legend()
-    if not os.path.exists(f'{exp_setup.rfmip_path}plots/{exp_setup.name}/'):
-            os.mkdir(f'{exp_setup.rfmip_path}plots/{exp_setup.name}/')
-    fig.savefig(f'{exp_setup.rfmip_path}plots/{exp_setup.name}/diffuse_flux.png', dpi=200)
+    if not os.path.exists(f"{exp_setup.rfmip_path}plots/{exp_setup.name}/"):
+        os.mkdir(f"{exp_setup.rfmip_path}plots/{exp_setup.name}/")
+    fig.savefig(
+        f"{exp_setup.rfmip_path}plots/{exp_setup.name}/diffuse_flux{index}.png", dpi=200
+    )
     plt.show()
 
+
 def main() -> None:
-    exp_setup = read_exp_setup(exp_name='test')
-    data = pyarts.xml.load(
-        f'{exp_setup.rfmip_path}output/{exp_setup.name}/combined_spectral_irradiance.xml') # wavelength, pressure, down-/upward
+    exp_setup = read_exp_setup(exp_name="test")
+    combined_data = pyarts.xml.load(
+        f"{exp_setup.rfmip_path}output/{exp_setup.name}/combined_spectral_irradiance.xml"
+    )  # wavelength, pressure, down-/upward
+    selected_data = pyarts.xml.load(
+        f"{exp_setup.rfmip_path}output/{exp_setup.name}/selected_spectral_irradiance.xml"
+    )  # wavelength, pressure, down-/upward
+    selected_heights = pyarts.xml.load(
+        f"{exp_setup.rfmip_path}output/{exp_setup.name}/selected_heights.xml"
+    )
     spectral_grid = np.linspace(
-        exp_setup.spectral_grid['min'], exp_setup.spectral_grid['max'], 
-        exp_setup.spectral_grid['n'], endpoint=True)
-    
-    if exp_setup.which_spectral_grid == 'wavelength':
-        f_grid = ty.physics.wavelength2frequency(spectral_grid*1e-9)[::-1]
-        irrad_diffuse, spectral_grid = ty.physics.perfrequency2perwavelength(data, f_grid)
-        irrad_diffuse_nm = irrad_diffuse*1e-9
-        spectral_grid_nm = spectral_grid*1e9
-        
-        plot_irradiance(irrad_diffuse_nm, spectral_grid_nm, exp_setup=exp_setup)
+        exp_setup.spectral_grid["min"],
+        exp_setup.spectral_grid["max"],
+        exp_setup.spectral_grid["n"],
+        endpoint=True,
+    )
 
-    elif exp_setup.which_spectral_grid == 'frequency':
-        plot_irradiance(data, spectral_grid, exp_setup=exp_setup)
+    if exp_setup.which_spectral_grid == "wavelength":
+        spectral_grid_nm = spectral_grid  # input is in nm
+        f_grid = ty.physics.wavelength2frequency(spectral_grid_nm * 1e-9)[::-1]
+        for i, profile in enumerate(selected_data):
+            irrad_diffuse, spectral_grid = ty.physics.perfrequency2perwavelength(
+                profile, f_grid
+            )
+            irrad_diffuse_nm = irrad_diffuse * 1e-9
 
-    elif exp_setup.which_spectral_grid == 'kayser':
-        f_grid = ty.physics.wavenumber2frequency(spectral_grid*1e2)
-        irrad_diffuse, spectral_grid = ty.physics.perfrequency2perwavenumber(data, f_grid)
-        irrad_diffuse_cm = irrad_diffuse*1e2
-        spectral_grid_cm = spectral_grid*1e-2
-        
-        plot_irradiance(irrad_diffuse_cm, spectral_grid_cm, exp_setup=exp_setup)
+            plot_irradiance(
+                selected_heights[i],
+                irrad_diffuse_nm,
+                spectral_grid_nm,
+                exp_setup=exp_setup,
+                index=i,
+            )
+
+    elif exp_setup.which_spectral_grid == "frequency":
+        for i, profile in enumerate(selected_data):
+            plot_irradiance(
+                selected_heights[i],
+                profile,
+                spectral_grid,
+                exp_setup=exp_setup,
+                index=i,
+            )
+
+    elif exp_setup.which_spectral_grid == "kayser":
+        spectral_grid_cm = spectral_grid  # input is in cm
+        f_grid = ty.physics.wavenumber2frequency(spectral_grid_cm * 1e2)
+        for i, profile in enumerate(selected_data):
+            irrad_diffuse, spectral_grid = ty.physics.perfrequency2perwavenumber(
+                profile, f_grid
+            )
+            irrad_diffuse_cm = irrad_diffuse * 1e2
+
+            plot_irradiance(
+                selected_heights[i],
+                irrad_diffuse_cm,
+                spectral_grid_cm,
+                exp_setup=exp_setup,
+                index=i,
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-# %%
