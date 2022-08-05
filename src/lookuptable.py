648d834c6f15910fa720d782ff5a6ffc2f09bc01
jpetersen <jon.petersen@web.de>
2022-08-05 09:36:46 +0200

    add parallelisim to the lookuptable calculation

--- src/lookuptable.py
+++ src/lookuptable.py
@@ -2,11 +2,43 @@ import os
 import pyarts
 import numpy as np
 import typhon as ty
+from itertools import repeat
+from multiprocessing import Pool
 
+import write_xml_input_data as input_data
 from experiment_setup import ExperimentSetup
 
+
+def calc_lookup_parallel(exp_setup, n_procs=8, recalculate=False):
+    with Pool(n_procs) as p:
+        p.starmap(calc_lookup, list(zip(repeat(exp_setup, n_procs), repeat(n_procs, n_procs), list(range(n_procs)), repeat(recalculate))))
+
+    # should only be used if the same species are in the luts!!
+    combine_luts(exp_setup, n_procs=n_procs)
+
+
+def combine_luts(exp_setup, n_procs=8):
+    lut_list = [
+        pyarts.xml.load(
+            f'{exp_setup.rfmip_path}lookup_tables/{exp_setup.name}/lookup_{str(i).zfill(2)}.xml'
+        ) for i in range(n_procs)
+    ]
+    main_lut = lut_list[0]
+    for lut in lut_list[1:]:
+        main_lut.f_grid = pyarts.arts.Vector(np.concatenate((main_lut.f_grid, lut.f_grid), axis=0))
+        main_lut.xsec = pyarts.arts.Tensor4(np.concatenate((main_lut.xsec, lut.xsec), axis=2))
+
+    pyarts.xml.save(main_lut, f'{exp_setup.rfmip_path}lookup_tables/{exp_setup.name}/lookup.xml')
+
+
+def calc_lookup(exp_setup, n_procs, proc_id, recalculate=False):
+    lut = LookUpTable(exp_setup, n_slice=n_procs, active_slice=proc_id)
+    lut.calculate(recalculate=recalculate)
+
 class LookUpTable():
-    def __init__(self, exp_setup, ws=None):
+    def __init__(self, exp_setup, ws=None, n_slice=None, active_slice=1):
+        self.active_slice = active_slice
+        self.n_slice = n_slice
         self.exp_setup = exp_setup
         self.new_ws = False
         if ws == None:
@@ -15,24 +47,25 @@ class LookUpTable():
         self.ws = ws
 
 
-    def calculate(self, recalculate=False):
+    def calculate(self, load_if_exist=False, recalculate=False):
         if self.check_existing_lut():
             if not recalculate:
                 print("The Lookup Table is already calculated. :)")
-                self.load_lut()
+                if load_if_exist:
+                    self.load_lut()
                 return
             print('The Lookuptable will be recalculated.')
 
         if self.new_ws:
             print('Necessary quantities are loaded.')
-            self.load_atmospheric_state()
+            self.lut_setup()
         print('The Lookup Table calculation is starting.')
         with ty.utils.Timer():
             self.calculate_lut()
         print('Finished with Lookup Table calculation.')
 
 
-    def load_atmospheric_state(self):
+    def lut_setup(self):
         self.ws.execute_controlfile("general/agendas.arts")
         self.ws.execute_controlfile("general/planet_earth.arts")
 
@@ -43,7 +76,7 @@ class LookUpTable():
         species = pyarts.xml.load(f"{self.exp_setup.rfmip_path}{self.exp_setup.input_folder}species.xml")
         self.add_species(species)
 
-         # Read a line file and a matching small frequency grid
+        # Read a line file and a matching small frequency grid
         self.ws.abs_lines_per_speciesReadSpeciesSplitCatalog(
         basename=f'{self.exp_setup.arts_data_path}arts-cat-data/lines/'
         )
@@ -52,19 +85,11 @@ class LookUpTable():
             basename=f'{self.exp_setup.arts_data_path}arts-cat-data/xsec/'
         )
 
-
-    def f_grid_from_spectral_grid(self):
-        if self.exp_setup.which_spectral_grid == 'frequency':  
-            self.ws.f_grid = np.linspace(self.exp_setup.spectral_grid['min'], self.exp_setup.spectral_grid['max'], self.exp_setup.spectral_grid['n'], endpoint=True)
-        elif self.exp_setup.which_spectral_grid == 'wavelength':
-            lam_grid = np.linspace(self.exp_setup.spectral_grid['min'], self.exp_setup.spectral_grid['max'], self.exp_setup.spectral_grid['n'], endpoint=True)*1e-9
-            self.ws.f_grid = ty.physics.wavelength2frequency(lam_grid)[::-1]
-        elif self.exp_setup.which_spectral_grid == 'kayser':
-            kayser_grid = np.linspace(self.exp_setup.spectral_grid['min'], self.exp_setup.spectral_grid['max'], self.exp_setup.spectral_grid['n'], endpoint=True)*1e2
-            self.ws.f_grid = ty.physics.wavenumber2frequency(kayser_grid)
+        self.ws.abs_lines_per_speciesCutoff(option='ByLine', value=750e9)
+        self.ws.abs_lines_per_speciesCompact()
 
 
-    def calculate_lut(self):
+    def calculate_lut(self, proc_id=None, n_proc=8):
         if not os.path.exists(f'{self.exp_setup.rfmip_path}lookup_tables/{self.exp_setup.name}/'):
             os.mkdir(f'{self.exp_setup.rfmip_path}lookup_tables/{self.exp_setup.name}/')
 
@@ -76,8 +101,11 @@ class LookUpTable():
 
         self.ws.propmat_clearsky_agendaAuto(use_abs_lookup=1)
 
-        self.ws.WriteXML('binary', self.ws.abs_lookup, f'{self.exp_setup.rfmip_path}lookup_tables/{self.exp_setup.name}/lookup.xml')
- 
+        if self.n_slice == None:
+            self.ws.WriteXML('binary', self.ws.abs_lookup, f'{self.exp_setup.rfmip_path}lookup_tables/{self.exp_setup.name}/lookup.xml')
+        else:
+            self.ws.WriteXML('binary', self.ws.abs_lookup, f'{self.exp_setup.rfmip_path}lookup_tables/{self.exp_setup.name}/lookup_{str(self.active_slice).zfill(2)}.xml')
+
 
     def load(self):
         """ Loads existing Lookup table and adjust it for the calculation. """
@@ -89,7 +117,23 @@ class LookUpTable():
 
         if not self.new_ws:
             self.ws.abs_lookupAdapt() # Adapts a gas absorption lookup table to the current calculation. Removes unnessesary freqs are species.
-    
+
+
+    def f_grid_from_spectral_grid(self):
+        if self.exp_setup.which_spectral_grid == 'frequency':
+            f_grid = np.linspace(self.exp_setup.spectral_grid['min'], self.exp_setup.spectral_grid['max'], self.exp_setup.spectral_grid['n'], endpoint=True)
+        elif self.exp_setup.which_spectral_grid == 'wavelength':
+            lam_grid = np.linspace(self.exp_setup.spectral_grid['min'], self.exp_setup.spectral_grid['max'], self.exp_setup.spectral_grid['n'], endpoint=True)*1e-9
+            f_grid = ty.physics.wavelength2frequency(lam_grid)[::-1]
+        elif self.exp_setup.which_spectral_grid == 'kayser':
+            kayser_grid = np.linspace(self.exp_setup.spectral_grid['min'], self.exp_setup.spectral_grid['max'], self.exp_setup.spectral_grid['n'], endpoint=True)
+            f_grid = ty.physics.wavenumber2frequency(kayser_grid)
+
+        if self.n_slice is not None:
+            self.ws.f_grid = get_chunk(f_grid, self.n_slice, self.active_slice)
+        else:
+            self.ws.f_grid = f_grid
+
 
     def add_species(self, species):
         # adding Line MIxing and Self Continuum for some species
@@ -100,12 +144,12 @@ class LookUpTable():
         if 'abs_species-O3' in species:
             replace_values(species, 'abs_species-O3', ['abs_species-O3', 'abs_species-O3-XFIT'])
         if 'abs_species-O2' in species:
-            replace_values(species, 'abs_species-O2', ['abs_species-O2', 'abs_species-O2-CIAfunCKDMT100'])   
+            replace_values(species, 'abs_species-O2', ['abs_species-O2', 'abs_species-O2-CIAfunCKDMT100'])
         if 'abs_species-N2' in species:
             replace_values(species, 'abs_species-N2', ['abs_species-N2', 'abs_species-N2-CIAfunCKDMT252', 'abs_species-N2-CIAfunCKDMT252'])
 
         species = [spec[12:] for spec in species]
-        
+
         self.ws.abs_speciesSet(species=species)
 
 
@@ -117,6 +161,11 @@ def replace_values(list_to_replace, item_to_replace, item_to_replace_with):
     return [item_to_replace_with if item == item_to_replace else item for item in list_to_replace]
 
 
+def get_chunk(lst, n_chunks, chunk_id):
+    slice_len = len(lst)//n_chunks
+    return lst[slice_len*chunk_id:slice_len*(chunk_id+1)]
+
+
 def main():
     exp = ExperimentSetup(
         name='lut',
@@ -126,12 +175,18 @@ def main():
         arts_data_path='/Users/jpetersen/rare/',
         solar_type='None',
         which_spectral_grid='wavelength',
-        spectral_grid={'min': 380, 'max': 780, 'n': 20},
-        species=['water_vapor', 'ozone', 'carbon_dioxide_GM', 'methane_GM', 'nitrous_oxide_GM'],
+        spectral_grid={'min': 380, 'max': 780, 'n': 300},
+        species=['water_vapor'],
         angular_grid={'N_za_grid': 20, 'N_aa_grid': 41, 'za_grid_type': 'linear_mu'}
     )
-    lut = LookUpTable(exp)
-    lut.calculate()
+    # input_data.create_input_data(exp_setup=exp)
+    with ty.utils.Timer():
+        calc_lookup_parallel(exp, 2)
+    
+    with ty.utils.Timer():
+        lut = LookUpTable(exp)
+        lut.calculate(recalculate=True)
+    
 
 if __name__ == '__main__':
     main()
